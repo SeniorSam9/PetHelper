6c8e2a1aab8c37b850078dce1f084f6db870ed82
ammar <ammar.alhmmad11@gmail.com>
2023-12-07 15:57:17 +0300

    profilepage layout finlized-ish

--- lib/pages/ProfilePage.dart
+++ lib/pages/ProfilePage.dart
@@ -8,8 +8,95 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Profile Page'),
+    return Scaffold(
+      body: DefaultTabController(
+        length: 2,
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              expandedHeight: 240.0,
+              floating: true,
+              pinned: true,
+              flexibleSpace: FlexibleSpaceBar(
+                background: Image(
+                  image: AssetImage('assets/images/profileBackground.jpg'),
+                  fit: BoxFit.cover,
+                ),
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Hello, Ammar!',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SliverPersistentHeader(
+              delegate: _SliverAppBarDelegate(
+                TabBar(
+                  tabs: [
+                    Tab(text: 'Adopted'),
+                    Tab(text: 'Reported'),
+                  ],
+                ),
+              ),
+              pinned: true,
+            ),
+            SliverFillRemaining(
+              child: TabBarView(
+                children: [
+                  ListView.builder(
+                    itemCount: 10,
+                    itemBuilder: (BuildContext context, int index) {
+                      return ListTile(
+                        title: Text('Adopted Item $index'),
+                      );
+                    },
+                  ),
+                  ListView.builder(
+                    itemCount: 10,
+                    itemBuilder: (BuildContext context, int index) {
+                      return ListTile(
+                        title: Text('Reported Item $index'),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
+  }
+}
+
+class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  final TabBar tabBar;
+
+  _SliverAppBarDelegate(this.tabBar);
+
+  @override
+  double get minExtent => tabBar.preferredSize.height;
+
+  @override
+  double get maxExtent => tabBar.preferredSize.height;
+
+  @override
+  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Container(
+      color: Theme.of(context).scaffoldBackgroundColor,
+      child: tabBar,
+    );
+  }
+
+  @override
+  bool shouldRebuild(_SliverAppBarDelegate oldDelegate) {
+    return false;
   }
 }
