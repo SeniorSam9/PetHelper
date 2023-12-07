050eac04411e1d6f4ea7923ebcea0fb2e1ca00bf
newCanvas1 <123483334+newCanvas1@users.noreply.github.com>
2023-12-07 22:18:07 +0300

    - edit

--- backend/firebase/firebase.js
+++ backend/firebase/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
-import { serviceAccount } from "./petHelper_creds.json";
+import serviceAccount from "./petHelper_creds.js";
 
 initializeApp({ credential: cert(serviceAccount) });
 
